f1378218c518ed2b607caf4fd33b43903a9c46b1
wangxuan007 <wangxuan8901@126.com>
2017-02-21 15:58:45 +0800

    Update basic0.py

--- basic0.py
+++ basic0.py
@@ -29,10 +29,46 @@ def reduce(function, sequence, initial=None): # real signature unknown; restored
     """
     pass
   
-eg: reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
-    def prod(L):
+eg: 1. reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+    2.def prod(L):
         def mulipy(a, b):
             return a *b
     return reduce(mulipy, L)
 print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
 >>> 3 * 5 * 7 * 9 = 945
+
+    3.CHAR_TO_FLOAT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '.': -1
+}
+
+def str2float(s):
+    nums = map(lambda ch: CHAR_TO_FLOAT[ch], s)
+    point = 0
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point = point * 10
+            return f + n / point
+    return reduce(to_float, nums, 0.0)
+
+print(str2float('0'))
+print(str2float('123.456'))
+print(str2float('123.45600'))
+print(str2float('0.1234'))
+print(str2float('.1234'))
+print(str2float('120.0034'))
